f69acd3aadceac3e936366172796277993063735
raisusmita <susmitathulung53@gmail.com>
2024-04-16 15:21:48 -0400

    initail commit to push project

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1982585.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 174406844, 174406856, 174406814, 174406840, 174406866, 174406842, 174406818, 174406833, 174406837, 174406839, 174406831, 174406864, 174406824, 174406829, 174406781, 174406789, 174406902, 174406798, 174406888, 174406881, 174406782 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/174406859/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/174406843/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/174406917/1/ammo.js', 'preload' : true},
+];
